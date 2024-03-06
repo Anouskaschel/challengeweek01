4c76e68fd701ef9d570e679b3bc797fd7f8e0581
JonathanHSR <162428924+JonathanHSR@users.noreply.github.com>
2024-03-06 09:52:20 +0100

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,22 @@
+
+import random
+
+
+capital_player_one = int()
+print(capital_player_one)
+capital_player_two = int()
+
+
+def roll_dice():
+    dice_one = random.randint(1, 6)
+    dice_two = random.randint(1, 6)
+    capital = dice_one + dice_two
+    return dice_one, dice_two, capital
+
+
+dice_one, dice_two, capital = roll_dice()
+capital_player_one += capital
+
+dice_one, dice_two, capital = roll_dice()
+capital_player_two += capital
+
