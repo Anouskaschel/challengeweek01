74f7ed6bb4aceea986193b8aa9a20634b66c113e
Jonathan Hekelaar <1080312@hr.nl>
2024-03-09 16:43:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def camper_items():
+    if owner[-1] == "1":
+        global camper_player_one
+
+    elif owner[-1] == "2":
+        global camper_player_two
+
+
+# When the user has to roll the dice
+print("Dit zijn de items die je hebt: \n")
+# print(camper_player_one) or print(camper_player_two)
+
